3b8e86efee5cb5ec20b36527b98830d3b4d00115
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-11-01 09:35:45 -0700

    Update Queue_New.java

--- Queue_New.java
+++ Queue_New.java
@@ -10,6 +10,16 @@ public class Queue_New {
         ob.offer(6);
         ob.add(8);
         System.out.println("queue" + ob);
-        // element() return
+        //element()-returns the head element of the queue and throw exception if the queue 
+		 //is empty.
+		//peek()-returns the head element of the queue and returns null if the queue is empty.
+		//create an variabale of int datatype
+		int a=ob.peek();
+		System.out.println("access element:"+a);
+		//remove()-remove the head of the queue and throw exception if the queue is empty.
+		//poll()-remove the head of the queue and returns if the queue is empty .
+		int b=ob.poll();
+		System.out.println("removed element:"+b);
+		System.out.println("updated queue:"+ob);
     }
 }
